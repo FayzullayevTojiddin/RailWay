d00d77360a96ebe0e2058ab4473f51d3ef3b4ec8
Azamat-akoooo <tojiddinfayzullaev981@gmail.com>
2025-12-07 19:42:28 +0500

    Add needed files for project

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\VoiceController;
 
+Route::get('', function () {
+    return redirect('/super');
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
